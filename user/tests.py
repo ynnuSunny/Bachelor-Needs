34a3dbdc328597a7c33114e9d2a12605dfa62cd7
Sunny Sutradhar <sunnysutradhar38@gmail.com>
2022-12-29 21:12:45 +0600

    Service Provider

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+import unittest
+from .views import *
+
+class TestConnection(unittest.TestCase):
+
+    connection = None
+
+    def setUp(self):
+        DBConnect
+        self.connection = mysql.connector.connect(**config)
+
+    def tearDown(self):
+        if self.connection is not None and self.connection.is_connected():
+            self.connection.close()
+
+    def test_connection(self):
+        self.assertTrue(self.connection.is_connected())
